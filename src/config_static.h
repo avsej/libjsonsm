253fbb250e067efc444886ef85b4ddcb044d01f6
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-07-11 10:54:33 +0300

    remove unnecessary header/symbol checks

--- src/config_static.h
+++ src/config_static.h
@@ -14,52 +14,16 @@
 #    include <stdint.h>
 #endif
 
-#ifdef HAVE_SYS_SOCKET_H
-#    include <sys/socket.h>
-#endif
-
-#ifdef HAVE_NETINET_IN_H
-#    include <netinet/in.h>
-#endif
-
-#ifdef HAVE_NETINET_TCP_H
-#    include <netinet/tcp.h>
-#endif
-
 #ifdef HAVE_INTTYPES_H
 #    include <inttypes.h>
-#endif
-
-#ifdef HAVE_NETDB_H
-#    include <netdb.h>
 #endif
 
 #ifdef HAVE_UNISTD_H
 #    include <unistd.h>
 #endif
 
-#ifdef HAVE_SYS_TIME_H
-#    include <sys/time.h>
-#endif
-
-#ifdef HAVE_SYS_UIO_H
-#    include <sys/uio.h>
-#endif
-
 #ifdef HAVE_STRINGS_H
 #    include <strings.h>
-#endif
-
-#ifdef HAVE_FCNTL_H
-#    include <fcntl.h>
-#endif
-
-#ifdef HAVE_DLFCN_H
-#    include <dlfcn.h>
-#endif
-
-#ifdef HAVE_ARPA_INET_H
-#    include <arpa/inet.h>
 #endif
 
 /* Standard C includes */
